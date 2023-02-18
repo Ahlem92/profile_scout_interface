0787698acd8247284f7921e5f6854b4eaec9dba6
Ahlem Belkrorissat <ahlem.belkrorissat@gmail.com>
2023-02-18 16:16:00 +0300

    new files

---
+++ app.py
@@ -0,0 +1,34 @@
+
+import streamlit as st
+
+
+st.markdown('''
+## Test
+''')
+
+'''
+## Here we would like to add some controllers in order to ask the user to select the player
+
+1. Let's ask for:
+- player full name
+
+'''
+
+'''
+## Once we have these, let's call our API in order to retrieve a prediction
+
+See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
+
+🤔 How could we call our API ? Off course... The `requests` package 💡
+'''
+
+'''
+
+2. Let's build a dictionary containing the parameters for our API...
+
+3. Let's call our API using the `requests` package...
+
+4. Let's retrieve the prediction from the **JSON** returned by the API...
+
+## Finally, we can display the prediction to the user
+'''
