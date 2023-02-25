ba5b1da2a668e63bf8a85b5c54e07e0cbdc8f69e
alix macgregor <macgregoralix@gmail.com>
2023-02-25 16:58:00 +0100

    Interface extremement belle

--- app.py
+++ app.py
@@ -12,23 +12,29 @@ from streamlit_extras.app_logo import add_logo
 def page_profile():
 
     st.title("_PROFILE SCOUT_ :soccer:")
+    st.write('**Done by :** blue[Ahlem Belko], blue[Ahlem Belko], blue[Ahlem Belko] et blue[Ahlem Belko] ')
 
     df = pd.read_csv('Fifa23_data.csv')
 
-    with st.form(key='params_for_api'):
-        st.header('Player Choice')
-        player_name = st.selectbox('Select a player :', df['Full Name'], key = 'player')
-        number_of_similar_profiles = st.number_input('Number of similar player :', 5)
+    with st.sidebar :
+        with st.form(key='params_for_api'):
+            st.header('Player Choice')
+            player_name = st.selectbox('Select a player :', df['Full Name'], key = 'player')
+            number_of_similar_profiles = st.number_input('Number of similar player :', 5)
 
-        st.header('Selection Criteria')
-        criteria = ["None", "Value(in Euro)", "Nationality", "Age", "Height", "Release Clause", "Contract Until"]
+            st.header('Selection Criteria')
 
+            value_euro = st.number_input('Select a budget :', min(df['Value(in Euro)']), max(df['Value(in Euro)']), max(df['Value(in Euro)']), key = 'budget')
+            age = st.number_input('Select a maximum Age :', min(df['Age']), max(df['Age']), max(df['Age']), key = 'age')
+            height = st.number_input('Select Height (cm) :', min(df['Height(in cm)']), key = 'height')
+            year = st.number_input('Select end of current contract :', 2023, 2100, 2100, key = 'year')
+            release_clause = st.number_input('Select Release Clause :', min(df['Release Clause']), max(df['Release Clause']), max(df['Release Clause']), key = 'rc')
 
-        filter_1 = st.selectbox('Select Criteria :', criteria, key = 'criteria1')
-        filter_2 = st.selectbox('Select Criteria', criteria, key = 'criteria2')
-        filter_3 = st.selectbox('Select Criteria', criteria, key = 'criteria3')
+            liste = list(df['Nationality'].unique())
+            liste.insert(0, 'None')
+            nationality = st.selectbox('Select a Nationality :', liste, key = 'nation')
 
-        submit_button = st.form_submit_button(label ='Get Similar Players')
+            submit_button = st.form_submit_button(label ='Get Similar Players')
 
     if submit_button:
         params = dict(
@@ -42,42 +48,21 @@ def page_profile():
 
         df2 = pd.read_json(json.dumps(results), orient ='index')
         df = df2.T
-        select = ['Similar Players','Value(in Euro)',"Positions Played", 'Best Position', 'Image Link','Age','Club Name','Contract Until','National Team Image Link', ]
-
-        if filter_1 != None :
-            st.session_state["filter_1"] = filter_1
-            if filter_1 == 'Value(in Euro)':
-                st.session_state['value_euro'] = st.number_input('Select a budget :', key = 'budget')
-            elif filter_1 == 'Age' or filter_1 == 'Contract Until' or filter_1 == 'Height':
-                st.write(f"Select {filter_1} :")
-                value_1 = st.slider(min(df[filter_1]), max(df[filter_1]), 1, key = f'{filter_1}')
-            elif filter_1 == 'Release Clause':
-                st.number_input('Select Release Clause :', key = 'rc')
-            elif filter_1 == 'Nationality' :
-                value_1 = st.selectbox('Select a Nationality :', df['Nationality'].unique(), key = 'nation')
-
-        st.write(st.session_state.value_euro)
-        st.write(st.session_state.filter_1)
-
-        # # Filtre 1
-        # if filter_1 != 'None' :
-        #     df = None
-
-        # # Filtre 2
-        # if filter_2 != 'None' :
-        #     df = None
-
-        # # Filtre 3
-        # if filter_3 != 'None' :
-        #     df = None
+        select = ['Similar Players','Value(in Euro)',"Positions Played", 'Best Position', 'Image Link','Age','Club Name','Contract Until','National Team Image Link' ]
 
         py_results = df[select]
 
+        default_image = 'https://www.pngkit.com/png/detail/126-1262807_instagram-default-profile-picture-png.png'
 
         st.header(py_results.index[0])
         col1, col2, col3 = st.columns([1, 1, 2])
         with col1:
-            st.write(f'{py_results["Value(in Euro)"].loc[py_results.index[0]]} €')
+            if py_results['Value(in Euro)'].loc[py_results.index[0]]//1000000 != 0 :
+                st.write(f'{round(py_results["Value(in Euro)"].loc[py_results.index[0]]/1000000, 1)}M €')
+            elif py_results['Value(in Euro)'].loc[py_results.index[0]]//1000 != 0 :
+                st.write(f'{int(py_results["Value(in Euro)"].loc[py_results.index[0]]/1000)}K €')
+            else :
+                st.write(f'{py_results["Value(in Euro)"].loc[py_results.index[0]]} €')
             st.image(py_results['Image Link'].loc[py_results.index[0]], width=70)
 
         with col2:
@@ -100,7 +85,12 @@ def page_profile():
             col1, col2, col3, col4 = st.columns([1, 1, 2,1])
 
             with col1:
-                st.write(f'{py_results["Value(in Euro)"].loc[py_results.index[i]]} €')
+                if py_results['Value(in Euro)'].loc[py_results.index[i]]//1000000 != 0 :
+                    st.write(f'{round(py_results["Value(in Euro)"].loc[py_results.index[i]]/1000000, 1)}M €')
+                elif py_results['Value(in Euro)'].loc[py_results.index[i]]//1000 != 0 :
+                    st.write(f'{int(py_results["Value(in Euro)"].loc[py_results.index[i]]/1000)}K €')
+                else :
+                    st.write(f'{py_results["Value(in Euro)"].loc[py_results.index[i]]} €')
                 st.image(py_results['Image Link'].loc[py_results.index[i]], width=70)
 
             with col2:
